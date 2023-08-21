01d4397ec37ff558222fde8a8d1023a65b1c8380
Shaikh Faizan <sfh8881212@gmail.com>
2023-08-21 16:31:19 +0500

    dones

---
+++ Shopping_cart/Controllers/BrandsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping_cart.Models;
+
+namespace Shopping_cart.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private readonly ShopDbContext db;
+
+        public BrandsController(ShopDbContext db)
+
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddBrand()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddBrand(Brand model)
+        {
+            var brand = new Brand()
+            {
+                BName = model.BName
+            };
+            db.brands.Add(brand);
+            db.SaveChanges();
+            return RedirectToAction("AddBrand");
+            
+        }
+
+        public IActionResult ViewBrand()
+        {
+            var getbrnad = db.brands.ToList();
+            return View(getbrnad);
+        }
+    }
+}
